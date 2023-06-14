b8a34ffca5ac297c7d4891c16c9c70982ff033f6
Mark Pekala <mark.pekala@nutanix.com>
2023-06-13 17:35:26 -0700

    fix configs for failover prep

---
+++ failover_manager/failover_manager.py
@@ -0,0 +1,9 @@
+
+
+class FailoverManager:
+    """
+    A class that wraps a precise failover command and records data
+    about the failover we'll need for analysis
+    """
+    def __init__(self):
+        pass
