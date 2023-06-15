918d21ad516518d6840f9db5cedf87ba14d6496b
Mark Pekala <mark.pekala@nutanix.com>
2023-06-15 11:01:44 -0700

    scraping for PNL

--- log_scraper/log_scraper.py
+++ log_scraper/log_scraper.py
@@ -18,6 +18,31 @@ def scrape_POL_events(fin: TextIOWrapper) -> List[POLEvent]:
             eix += 1
     return result
 
+def scrape_PNL_events(fin: TextIOWrapper) -> List[PNLEvent]:
+    result = []
+    last_follow_ix = -1
+    # First get the ix of the last follow
+    follow = PNLOrder[0]
+    for ix, line in enumerate(fin.readlines()):
+        if follow.marker in line:
+            last_follow_ix = ix
+    # Then we can just speedrun sequential
+    eix = 0
+    for ix, line in enumerate(fin.readlines()):
+        if ix < last_follow_ix:
+            continue
+        if eix >= len(PNLOrder):
+            break
+        marker = PNLOrder[eix].marker
+        if marker in line:
+            event = copy.deepcopy(PNLOrder[eix])
+            event.timestamp = datetime.strptime(line[:23] + "000", "%Y-%m-%d %H:%M:%S,%f")
+            result.append(event)
+            eix += 1
+    return result
+        
+    
+
 if __name__ == "__main__":
     with open("../patroni/logs/patroni.log", "r") as fin:
-        print(scrape_POL_events(fin))
+        print(scrape_PNL_events(fin))
